da2a59ac75af554e44d4ea86c6a7af362e435489
Ismat-Samadov <ismetsemedov@gmail.com>
2023-10-21 14:18:45 +0400

    check

---
+++ JobScraper.py
@@ -0,0 +1,205 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import pandas as pd
+import requests
+import urllib3
+from bs4 import BeautifulSoup
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+class JobScraper:
+    def __init__(self):
+        self.data = None
+
+    def scrape_azercell(self):
+        url = "https://www.azercell.com/az/about-us/career.html"
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+            vacancies_section = soup.find("section", class_="section_vacancies")
+            if vacancies_section:
+                job_listings = vacancies_section.find_all("a", class_="vacancies__link")
+                job_titles = []
+                job_links = []
+                for job in job_listings:
+                    job_title = job.find("h4", class_="vacancies__name").text
+                    job_link = job["href"]
+                    job_titles.append(job_title)
+                    job_links.append(job_link)
+                df = pd.DataFrame({
+                    'company': 'azercell',
+                    "vacancy": job_titles,
+                    "apply_link": job_links
+                })
+                return df
+            else:
+                print("Vacancies section not found on the page.")
+        else:
+            print("Failed to retrieve the page. Status code:", response.status_code)
+
+
+    def scrape_pashabank(self):
+        url = "https://careers.pashabank.az/az/page/vakansiyalar?q=data&branch="
+        response = requests.get(url)
+        vacancy_list = []
+        apply_link_list = []
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            job_listings = soup.find_all('div', class_='what-we-do-item')
+            for listing in job_listings:
+                job_title = listing.find('h3').text
+                apply_link = listing.find('a')['href']
+                vacancy_list.append(job_title)
+                apply_link_list.append(apply_link)
+        else:
+            print(f"Failed to retrieve the page. Status code: {response.status_code}")
+        data = {
+            'company': 'pashabank',
+            'vacancy': vacancy_list,
+            'apply_link': apply_link_list
+        }
+        df = pd.DataFrame(data)
+        df = df.drop_duplicates(subset=['company', 'vacancy', 'apply_link'])
+        return df
+
+
+    def scrape_azerconnect(self):
+        url = "https://www.azerconnect.az/careers"
+        response = requests.get(url, verify=False)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            job_listings = soup.find_all('div', class_='CollapsibleItem_content__KGo_x')
+
+            job_data = []
+            apply_links = []
+            for job in job_listings:
+                job_details = job.text
+                job_data.append(job_details)
+                apply_link = job.find('a', class_='Button_button-blue__0wZ4l')['href']
+                apply_links.append(apply_link)
+            df = pd.DataFrame({'company': 'azerconnect',
+                            'vacancy': job_data,
+                            'apply_link': apply_links})
+            return df
+
+        else:
+            print("Failed to retrieve the web page.")
+            return None
+
+
+    def scrape_abb(self):
+        base_url = "https://careers.abb-bank.az/api/vacancy/v2/get"
+        job_vacancies = []
+        page = 1
+
+        while True:
+            params = {"page": page}
+            response = requests.get(base_url, params=params)
+
+            if response.status_code == 200:
+                data = response.json()["data"]
+
+                if not data:
+                    break
+
+                for item in data:
+                    title = item.get("title")
+                    url = item.get("url")
+                    job_vacancies.append({"company": "abb", "vacancy": title, "apply_link": url})
+
+                page += 1
+            else:
+                print(f"Failed to retrieve data for page {page}. Status code: {response.status_code}")
+                break
+
+        df = pd.DataFrame(job_vacancies)
+        return df
+
+    def scrape_rabitebank(self):
+            url = "https://www.rabitabank.com/insan-resurslari/vakansiyalar"
+            response = requests.get(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                job_titles = []
+                job_links = []
+
+                for element in soup.select('#vacancies > div > a'):
+                    job_titles.append(element.text.strip())
+                    job_links.append(element['href'])
+
+                data = {
+                    'company': 'rabitebank',
+                    'vacancy': job_titles,
+                    'apply_link': job_links,
+                }
+                df = pd.DataFrame(data)
+
+                return df
+
+            else:
+                print("Failed to retrieve the page. Status code:", response.status_code)
+                return None
+
+    def send_email(self, data_frame, to_email):
+        html_table = data_frame.to_html(index=False)
+        from_email = "ismetsemedov@gmail.com"
+        email_password = "lmjareknmmweotsp"
+        subject = "🔥🔥🔥 J O B S 🔥🔥🔥"
+        message = MIMEMultipart()
+        message["From"] = from_email
+        message["To"] = to_email
+        message["Subject"] = subject
+        message.attach(MIMEText(html_table, "html"))
+        try:
+            server = smtplib.SMTP("smtp.gmail.com", 587)
+            server.starttls()
+            server.login(from_email, email_password)
+            server.sendmail(from_email, to_email, message.as_string())
+            server.quit()
+            print("Email sent successfully.")
+        except Exception as e:
+            print("Email could not be sent. Error:", str(e))
+
+    def get_data(self):
+        abb_df = self.scrape_abb()
+        azerconnect_df = self.scrape_azerconnect()
+        pashabank_df = self.scrape_pashabank()
+        azercell_df = self.scrape_azercell()
+        rabitebank_df = self.scrape_rabitebank()
+
+        self.data = pd.concat([pashabank_df, azerconnect_df, azercell_df, abb_df, rabitebank_df], ignore_index=True)
+
+    def filter_and_send_emails(self):
+        ismat = "ismetsemedli@mail.ru"
+        nigar = "nigar.ly77@gmail.com"
+        polad = "polad55@rambler.ru"
+        cavid = "xasayev1996@gmail.com"
+        kamal = "kamalkhalilov7@gmail.com"
+        vusal = "shukurovvusal@gmail.com"
+        nasib = "suleymanovnasib@gmail.com"
+        azar  = "azer14480@gmail.com"
+        mubu = "Abdiyev-mubariz@mail.ru"
+        if self.data is not None:
+            df_data = self.data[self.data['vacancy'].str.contains("data", case=False)].reset_index(drop=True)
+            df_audit = self.data[self.data['vacancy'].str.contains("audit", case=False)].reset_index(drop=True)
+            df_scrum = self.data[self.data['vacancy'].str.contains("scrum", case=False)].reset_index(drop=True)
+            df_business = self.data[self.data['vacancy'].str.contains("biznes", case=False)].reset_index(drop=True)
+            df_fraud = self.data[self.data['vacancy'].str.contains("fraud", case=False)].reset_index(drop=True)
+            if not df_data.empty:
+                self.send_email(df_data,mubu)
+                self.send_email(df_data,ismat )
+                self.send_email(df_data,kamal )
+            if not df_business.empty:
+                self.send_email(df_business,kamal)
+            if not df_scrum.empty:
+                self.send_email(df_scrum,azar)
+            if not df_audit.empty:
+                self.send_email(df_audit, polad)
+                self.send_email(df_audit, nasib)
+            if not df_fraud.empty:
+                self.send_email(df_fraud,cavid)
+
+if __name__ == "__main__":
+    job_scraper = JobScraper()
+    job_scraper.get_data()
+    job_scraper.filter_and_send_emails()
--- README.md
+++ README.md
@@ -1,2 +1,116 @@
-# vacancy_notifier
-send vacancies with email
+Certainly! Here's a detailed README template for your project. You can use this as a starting point and customize it to fit your project's specific needs:
+
+---
+
+# Job Scraper and Email Notifier
+
+This Python script is designed to scrape job listings from various company websites and send email notifications to specific recipients based on keywords found in the job listings. It provides a flexible way to monitor job opportunities that match your criteria.
+
+## Table of Contents
+
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Usage](#usage)
+- [Configuration](#configuration)
+  - [Email Configuration](#email-configuration)
+  - [Recipient Configuration](#recipient-configuration)
+- [Customization](#customization)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- Scrapes job listings from multiple company websites.
+- Filters job listings based on specific keywords (e.g., "data," "audit," "scrum").
+- Sends email notifications to different recipients for each keyword.
+- Uses Pandas for data management and email.mime for email creation.
+
+## Getting Started
+
+### Prerequisites
+
+Before running the script, make sure you have the following prerequisites installed:
+
+- Python 3.7 or higher
+- Required Python packages (install them using `pip`):
+  - `beautifulsoup4` (4.12.2)
+  - `pandas` (2.1.1)
+  - `requests` (2.31.0)
+  - `urllib3` (1.26.16)
+  - `brotli` (1.1.0)
+
+### Installation
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/Ismat-Samadov/vacancy_notifier.git
+   cd job-scraper
+   ```
+
+2. Create a virtual environment (recommended) and activate it:
+
+   ```bash
+   python -m venv venv
+   source venv/bin/activate  # On Windows, use: venv\Scripts\activate
+   ```
+
+3. Install the required Python packages from the `requirements.txt` file:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## Usage
+
+To use the job scraper and email notifier, follow these steps:
+
+1. Edit the script to customize it for your specific needs.
+
+2. Set up your email configuration and recipient configuration (see [Configuration](#configuration)).
+
+3. Run the script:
+
+   ```bash
+   python main.py
+   ```
+
+The script will scrape job listings, filter them based on your criteria, and send email notifications to the specified recipients.
+
+## Configuration
+
+### Email Configuration
+
+In the script, you'll need to configure your email settings to send notifications. This includes the sender's email address, email password, and SMTP server settings.
+
+```python
+from_email = "your_email@gmail.com"
+email_password = "your_email_password"
+```
+
+Make sure to secure your email credentials and consider using environment variables for enhanced security.
+
+### Recipient Configuration
+
+In the `filter_and_send_emails` method, you can specify recipients for different job categories. Customize this section to match your requirements.
+
+```python
+if not df_data.empty:
+    self.send_email(df_data, "recipient1@email.com")
+```
+
+## Customization
+
+You can customize the script to scrape job listings from additional company websites or modify the keywords used for filtering. Additionally, you can extend or modify the email notification logic to match your preferences.
+
+## Contributing
+
+Contributions are welcome! If you have ideas for improvements or new features, please open an issue or submit a pull request.
+
+## License
+<!-- 
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details. -->
+
+---
